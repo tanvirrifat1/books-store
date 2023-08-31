55b0ccc4b8ddf16dbcae4078d05c249da0bd214d
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2023-08-31 22:50:04 +0600

    added auth ENUM

--- src/app/modules/User/user.route.ts
+++ src/app/modules/User/user.route.ts
@@ -1,15 +1,22 @@
 import express from 'express';
+import { ENUM_USER_ROLE } from '../../../enums/user';
+import auth from '../../middlewares/auth';
 import validateRequest from '../../middlewares/validateRequest';
 import { UserController } from './user.controller';
 import { UserValidation } from './user.validation';
 
 const router = express.Router();
 
-router.get('/', UserController.getAllFromDb);
-router.get('/:id', UserController.getSingleDataFromDb);
-router.delete('/:id', UserController.deletedData);
+router.get('/', auth(ENUM_USER_ROLE.ADMIN), UserController.getAllFromDb);
+router.get(
+  '/:id',
+  auth(ENUM_USER_ROLE.ADMIN),
+  UserController.getSingleDataFromDb
+);
+router.delete('/:id', auth(ENUM_USER_ROLE.ADMIN), UserController.deletedData);
 router.patch(
   '/:id',
+  auth(ENUM_USER_ROLE.ADMIN),
   validateRequest(UserValidation.update),
   UserController.update
 );
